ed799dc7177cba2063bafe01cb0c4c58ea369320
typist95616 <typist95616@gmail.com>
2022-08-25 18:42:26 +1200

    add Knight model and movement animation, health system

---
+++ Assets/Script/Health/Health.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+   [SerializeField] private float startingHealth;
+
+   public float currentHealth { get; private set; }
+   private Animator anim;
+
+   private void Awake(){
+        currentHealth = startingHealth;
+        anim = GetComponent<Animator>();
+   }
+
+   public void TakeDamage(float _damage){
+        currentHealth = Mathf.Clamp(currentHealth - _damage, 0, startingHealth);
+        if(currentHealth > 0){
+            anim.SetTrigger("Hurt");
+        } else {
+            anim.SetTrigger("Death");
+        }
+   }
+
+   public void Update(){
+        if(Input.GetKeyDown("z")){
+            TakeDamage(1);
+        }
+   }
+}

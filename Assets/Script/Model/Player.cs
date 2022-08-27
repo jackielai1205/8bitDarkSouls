54951becc105962e67a9753168d170e9d108405e
Meateater02 <dqh.phan@gmail.com>
2022-08-27 15:00:13 +1200

    Added status system to player script

--- Assets/Script/Model/Player.cs
+++ Assets/Script/Model/Player.cs
@@ -4,7 +4,17 @@ using UnityEngine;
 
 public class Player : Character
 {
-    public int stamina;
+    public int maxHealth = 200;
+	public int currentHealth;
+	public int maxStamina = 150;
+	public int currentStamina;
+
+	private Animator animState;
+	private WaitForSeconds regenTick = new WaitForSeconds(0.1f);
+	private Coroutine regen;
+
+	public HealthBar2 healthBar;
+	public StaminaBar staminaBar;
     public Inventory inventory;
 
     void Dodge()
@@ -35,13 +45,26 @@ public class Player : Character
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
+		healthBar.SetMaxHealth(maxHealth);
+
+		currentStamina = maxStamina;
+		staminaBar.SetMaxStamina(maxStamina);
+		this.animState = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("z"))
+		{
+			TakeDamage(15);
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			UseStamina(10);
+		}
     }
 
     public void Walk()
@@ -63,4 +86,53 @@ public class Player : Character
     {
         throw new System.NotImplementedException();
     }
+
+    public void TakeDamage(int _damage)
+	{
+        if(currentHealth - _damage > 0){
+			currentHealth -= _damage;
+            this.animState.SetTrigger("Hurt");
+			healthBar.SetHealth(currentHealth);
+        } 
+		else 
+		{
+			currentHealth = 0;
+			healthBar.SetHealth(currentHealth);
+            this.animState.SetTrigger("Death");
+        }
+	}
+
+    public void UseStamina(int stamina)
+	{
+		if(currentStamina - stamina >= 0)
+		{
+			currentStamina -= stamina;
+			staminaBar.SetStamina(currentStamina);
+
+			if(regen != null)
+			{
+				StopCoroutine(regen);
+			}
+
+			regen = StartCoroutine(RegenStamina());
+		}
+		else
+		{
+			Debug.Log("Not enough Stamina");
+		}
+	}
+
+	private IEnumerator RegenStamina()
+	{
+		yield return new WaitForSeconds(2);
+
+		while(currentStamina < maxStamina)
+		{
+			currentStamina += 1;
+			Debug.Log("stamina increasing by: "+ currentStamina);
+			staminaBar.SetStamina(currentStamina);
+			yield return regenTick;
+		}
+		regen = null;
+	}
 }

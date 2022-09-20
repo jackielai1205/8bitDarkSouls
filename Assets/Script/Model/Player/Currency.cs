27260adb8d1e9da6f9f85d27ccc34a0277cb8355
Meateater02 <dqh.phan@gmail.com>
2022-09-20 19:30:31 +1200

    WIP: currency display

--- Assets/Script/Model/Player/Currency.cs
+++ Assets/Script/Model/Player/Currency.cs
@@ -1,9 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Currency : MonoBehaviour
 {
+    public GameObject currencyText;
+    public int currencyCoins;
+    public Player player;
+
+    void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.gameObject.tag == "Player")
+        {
+        currencyCoins += 1;
+        currencyText.GetComponent<TextMeshPro>().text = "Coins: " + currencyCoins;
+        Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {

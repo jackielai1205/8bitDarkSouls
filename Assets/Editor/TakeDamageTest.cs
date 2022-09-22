fe3b73ef5de869ebab6ee0b0275582802d12b15a
typist95616 <typist95616@gmail.com>
2022-09-22 13:37:49 +1200

    fix merge conflict

---
+++ Assets/Editor/TakeDamageTest.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+public class TakeDamageTest
+{
+
+    // [Test]
+    // public void TakeDamage_Test()
+    // {
+    //
+    //     Player player = new Player();
+    //     
+    //     int damage = 10;
+    //     player.currentHealth = 0;
+    //     player.m_blocking = false;
+    //     player.TakeDamage(damage);
+    //     
+    //     Assert.That(player.m_dead, Is.EqualTo(false));
+    //     
+    // }
+    
+}

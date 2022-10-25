c541171383df245145ac51d99afb66bc501c33b1
Jackie Lai <jackie12051995@gmail.com>
2022-10-25 16:21:28 +1300

    fixed the enemy health bar

--- Assets/Script/Model/Enemy/EnemyHealthBar.cs
+++ Assets/Script/Model/Enemy/EnemyHealthBar.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,16 +8,22 @@ namespace Script.Model.Enemy
     public class EnemyHealthBar : MonoBehaviour
     {
         public Slider slider;
-
         public Color low;
-
         public Color high;
-
         public Vector3 offset;
+        public RectTransform fillArea;
 
         public virtual void SetHealth(int health, int maxHealth)
         {
             slider.gameObject.SetActive(health < maxHealth);
+            if (health <= 0)
+            {
+                fillArea.gameObject.SetActive(false);
+            }
+            else
+            {
+                slider.value = health;
+            }
             slider.value = (health <= 0) ? 0 : health;
             slider.maxValue = maxHealth;
             slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(low, high, slider.normalizedValue);
